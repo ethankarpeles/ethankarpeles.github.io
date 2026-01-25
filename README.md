a165f299c636a995cc1db7fc9eed46b790f25972
Ethan Karpeles <ethankarpeles@gmail.com>
2026-01-24 21:38:58 -0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ethan Karpeles Personal Website
-This is a personal website I am making for myself with React, TypeScript, and GitHub Pages!
--- src/routes/articles.ts
+++
@@ -1,9 +0,0 @@
-import MinecraftCalculator from '@pages/Musings/MinecraftCalculator';
-import RationalPoints from '@pages/Musings/RationalPointsOnEllipticCurves.tsx';
-import ReductionsOfEllipticCurves from '@pages/Musings/ReductionsOfEllipticCurves.tsx';
-
-export const articles = [
-    { path: 'rational-points-on-elliptic-curves', title: 'Rational Points on Elliptic Curves', component: RationalPoints },
-    { path: 'reductions-of-elliptic-curves', title: 'Reductions of Elliptic Curves and Their Galois Representations', component: ReductionsOfEllipticCurves },
-    { path: 'minecraft-calculator', title: 'Minecraft Calculator', component: MinecraftCalculator },
-];
