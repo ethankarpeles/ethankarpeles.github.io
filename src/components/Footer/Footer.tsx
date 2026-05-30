ebb0b3c69b40abda51523473269df6b7c30d1e69
Ethan Karpeles <ethankarpeles@gmail.com>
2026-05-29 22:28:59 -0500

    Remove text from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
-    return <footer className={styles.footer}>
-        Created with React.js and TypeScript
-    </footer>
+  return <footer className={styles.footer}>
+  </footer>
 }
