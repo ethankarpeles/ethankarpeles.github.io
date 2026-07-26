62bb56f8e96456b338f790fea6ed1ac27c47b014
Ethan Karpeles <ethankarpeles@gmail.com>
2026-07-26 03:35:32 -0500

    Musings Page Styling
    Plus some prettier config stuff.

--- src/pages/Musings/Library.tsx
+++ src/pages/Musings/Library.tsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import { articles } from "@routes/articles";
+import styles from "./Library.module.css";
 import Footer from "@components/Footer/Footer.tsx";
 
 export default function Library() {
   return (
     <>
-      <article>
+      <article className={styles.library}>
         <h1>Musings</h1>
-        <ul>
-          {articles.map((a) => (
-            <li key={a.path}>
-              <Link to={a.path}>{a.title}</Link>
-            </li>
+        <div className={styles.cards}>
+          {articles.map((article) => (
+            <Link key={article.path} to={article.path} className={styles.card}>
+              <div className={styles.title}>{article.title}</div>
+              <div className={styles.description}>{article.description}</div>
+            </Link>
           ))}
-        </ul>
+        </div>
       </article>
       <Footer />
     </>
--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -7,21 +7,28 @@ export const articles = [
   {
     path: "jacobian-conjecture-counterexample",
     title: "On the Jacobian Conjecture Counterexample",
+    description:
+      "An accessible breakdown of Levent Alpöge’s recent multivariable polynomial map refuting the conjecture.",
     component: JacobianConjectureCounterexample,
   },
   {
     path: "rational-points-on-elliptic-curves",
     title: "Rational Points on Elliptic Curves",
+    description:
+      "An introduction to Pythagorean triples, Fermat's Last Theorem, and the algebraic structure of elliptic curves.",
     component: RationalPoints,
   },
   {
     path: "reductions-of-elliptic-curves",
     title: "Reductions of Elliptic Curves and Their Galois Representations",
+    description: "A poster presented at UNT Scholars Day in 2025.",
     component: ReductionsOfEllipticCurves,
   },
   {
     path: "minecraft-calculator",
     title: "Minecraft Calculator",
+    description:
+      "An 8-bit redstone calculator built in Minecraft, inspired by Ben Eater's breadboard computer.",
     component: MinecraftCalculator,
   },
 ];
